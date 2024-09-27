b1f178872816135aab3da4b8239fe429afae0414
tono || トノ <64560893+smtono@users.noreply.github.com>
2024-09-27 03:23:15 -0500

    Start adduser

--- src/ui/cli.py
+++ src/ui/cli.py
@@ -15,14 +15,48 @@ import pathlib
 import json
 import logging
 import sys
+from database.sql.database import Database
+
+ctx = {"database": ""}
 
 
-def help_lingua():
-    print("help")
+def help_lingua(command_help):
+    """Return help on command"""
+    command_help = command_help[0]
+    if not command_help:
+        print(
+            "Lingua bot is a language learning bot meant to aid and assist in"
+            "\nthe creation of flashcards, generative sentences, and overall"
+            "\npersonalized learning experience for the user."
+            "\nThis project is one of the first steps to test basic functionality"
+            "\nof the future bot, through an interface that is easier"
+            "\nto access than running the bot itself.\n\n"
+            "Through this CLI the following commands can be run:"
+            "\n\tadduser"
+            "\n\taddsentence"
+            "\n\ttestquiz\n"
+            "Enter a command to learn more"
+        )
+        command_help = input(">>> ")
+        print("")
+
+    if command_help == "adduser":
+        print("\nadduser")
+        print("Simulates adding new user to the database based on given information\n")
+    if command_help == "addsentence":
+        print("\naddsentence")
+        print(
+            "Simulates adding a sentence by breaking down word by word and adding to a DB"
+        )
+    if command_help == "testquiz":
+        print("\ntestquiz")
+        print("Simulates creating a quiz with entries in the DB")
+    print("")
 
 
-def add_user():
+def add_user(info):
     print("adduser")
+    print(f"Adding user with info: {info}")
 
 
 def add_sentence():
@@ -50,7 +84,8 @@ def parse_input(text):
         "testquiz": test_quiz,
     }
 
-    commands[text]()
+    command_args = text.split()
+    commands[command_args[0]](command_args[1:])
 
 
 def cli():
@@ -70,7 +105,7 @@ def cli():
 
     # Test config
     # Set up ctx
-    ctx = {"database": []}  # type: ignore
+    ctx["database"] = Database("users_test")
     # config = Configuration(ctx)
 
     # Database
